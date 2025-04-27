683c7fffb4aef28f65b78e6a1797112417d0067c
clawrence7408 <clawrence7408@gmail.com>
2025-04-27 11:34:27 -0400

    Fixing close button

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
 
